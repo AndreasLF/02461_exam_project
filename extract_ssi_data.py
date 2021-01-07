87669a00a90c65ab6257c680f90a72f14d5a7b62
August Tollerup <augusttollerup@Augusts-MacBook-Pro.local>
2021-01-07 12:13:54 +0100

    Feat: Extract ssi data function and data plot function

--- extract_ssi_data.py
+++ extract_ssi_data.py
@@ -1,21 +1,42 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
-weekly_test_regions_file = "data/Data-Epidemiologiske-Rapport-04012021-ja21/Test_regioner.csv"
-test_pos_over_time_file = "data/Data-Epidemiologiske-Rapport-04012021-ja21/Test_pos_over_time.csv"
-
-weekly_test_regions = pd.read_csv(weekly_test_regions_file, sep=";")
-test_pos_over_time = pd.read_csv(test_pos_over_time_file, sep=";")
-
-df = test_pos_over_time[["Date", "PosPct"]].iloc[:-2]
-print(df)
-
-pd.set_option("display.max_columns", 400)
-# print("weekly test regions")
-# print(weekly_test_regions)
-
-df.plot(x ="Date", y = "PosPct", kind = "bar")
-plt.show()
+import seaborn as sns
+from pylab import rcParams
+from matplotlib import rc
+from pandas.plotting import register_matplotlib_converters
 
 
+def extract_ssi_data():
+    """ Extract new daily positive cases
+    
+    Return:
+        panda dataframe 
+     """
+    test_pos_over_time_file = "data/Data-Epidemiologiske-Rapport-04012021-ja21/Test_pos_over_time.csv"
 
+    # Read data
+    test_pos_over_time = pd.read_csv(test_pos_over_time_file, sep=";", decimal=",")
+
+    # Remove last two rows
+    df = test_pos_over_time["NewPositive"].iloc[:-2]
+
+    # Set index to datetime
+    df.index = pd.to_datetime(test_pos_over_time["Date"].iloc[:-2])
+
+    # Remove . from data
+    df = df.str.replace(".","").astype(int)
+
+    return df
+
+def data_exploration(df):
+    """ Plot data 
+    params: 
+        df (panda dataframe)
+    """
+    df.plot()
+    plt.title("Daily covid cases");
+    plt.show()
+
+
+data_exploration(extract_ssi_data())
+    
