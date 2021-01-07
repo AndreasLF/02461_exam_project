4ee51df68aee789cd4d5e7fa1d8281fbb8623336
August Tollerup <augusttollerup@Augusts-MacBook-Pro.local>
2021-01-07 13:15:54 +0100

    Feat: Added LSTM Training Model

--- lstm-model.py
+++ lstm-model.py
@@ -1,4 +1,5 @@
-import torch, os 
+import torch, os
+from math import floor
 import numpy as np 
 import pandas as pd 
 from tqdm import tqdm 
@@ -12,7 +13,172 @@ from sklearn.preprocessing import MinMaxScaler
 from pandas.plotting import register_matplotlib_converters
 from torch import nn, optim
 
+# Our module
+from extract_ssi_data import extract_ssi_data
+
+# Import SSI Data
+df = extract_ssi_data()
 
 RANDOM_SEED = 42
 np.random.seed(RANDOM_SEED)
 torch.manual_seed(RANDOM_SEED)
+
+########################
+#   Preproccessing     #
+########################
+
+# Split data 80 % traning 20 % test
+test_data_size = int(floor(len(df)*0.20))
+train_data = df[:-test_data_size]
+test_data = df[-test_data_size:]
+
+# Data skal squishes til min-max [0:1]
+# Dette gør vi for at optimere training speed (blandt andet også derfor man bruger tanh)
+# bruger sckit MinMaxScaler
+
+scaler = MinMaxScaler()
+
+scaler = scaler.fit(np.expand_dims(train_data, axis=1))
+
+train_data = scaler.transform(np.expand_dims(train_data, axis=1))
+
+test_data = scaler.transform(np.expand_dims(test_data, axis=1))
+
+# train_data.shape
+
+# Sequences are too large, we'll 
+
+def create_sequences(data, seq_length):
+    """ Create sequences from the data 
+    
+    params: 
+        data (dataframe): The data you want to split in sequences
+        seq_length (int): the length of the sequences
+    
+    return:
+       
+    """
+    
+    xs = []
+    ys = []
+    for i in range(len(data)-seq_length-1):
+        x = data[i:(i+seq_length)]
+        y = data[i+seq_length]
+        xs.append(x)
+        ys.append(y)
+        
+    return np.array(xs), np.array(ys)
+
+
+# Vi opdeler vores data i sekvenser på 5 datapoints. Dette skal nok forøges da vi har meget mere end 41 datapunkter
+seq_length = 20
+X_train, y_train = create_sequences(train_data, seq_length)
+X_test, y_test = create_sequences(test_data, seq_length)
+
+# Vi konverterer træningsdata og testdata til fra numpy til pytorch tensors
+X_train = torch.from_numpy(X_train).float()
+y_train = torch.from_numpy(y_train).float()
+
+X_test = torch.from_numpy(X_test).float()
+y_test = torch.from_numpy(y_test).float()
+
+class CoronaProphet(nn.Module):
+    def __init__(self, n_features, n_hidden, seq_len, n_layers=2):
+        super(CoronaProphet, self).__init__()
+
+        self.n_hidden = n_hidden
+        self.seq_len = seq_len
+        self.n_layers = n_layers
+
+        self.lstm = nn.LSTM(
+            input_size = n_features,
+            hidden_size = n_hidden,
+            num_layers = n_layers,
+            dropout = 0.5
+        )
+
+        self.linear = nn.Linear(in_features=n_hidden, out_features=1)
+
+    
+    def reset_hidden_state(self):
+        self.hidden = (
+            torch.zeros(self.n_layers, self.seq_len, self.n_hidden),
+            torch.zeros(self.n_layers, self.seq_len, self.n_hidden)
+        )
+
+    def forward(self, sequences):
+        lstm_out, self.hidden = self.lstm(
+            sequences.view(len(sequences), self.seq_len, -1), 
+            self.hidden
+        )
+        last_time_step = \
+            lstm_out.view(self.seq_len, len(sequences), self.n_hidden)[-1]
+        y_pred = self.linear(last_time_step)
+
+        return y_pred
+
+def train_model(
+    model, 
+    train_data,
+    train_labels,
+    test_data = None,
+    test_labels = None
+):
+
+    loss_fn = torch.nn.MSELoss(reduction="sum")
+
+    optimiser = torch.optim.Adam(model.parameters(), lr=1e-3)
+    num_epochs = 60
+
+    train_hist = np.zeros(num_epochs)
+    test_hist = np.zeros(num_epochs)
+
+    for t in range(num_epochs):
+        print("Epoch: "+str(t))
+        model.reset_hidden_state()
+
+        y_pred = model(X_train)
+
+        loss = loss_fn(y_pred.float(), y_train)
+
+        if test_data is not None:
+            print("Test Data is not None")
+            with torch.no_grad():
+                y_test_pred = model(X_test)
+                test_loss = loss_fn(y_test_pred.float(), y_test)
+
+            test_hist[t] = test_loss.item()
+
+            if t % 10 == 0:
+                print(f'Epoch {t} train loss: {loss.item()} test loss: {test_loss.item()}')
+        
+        elif t % 10 == 0:
+            print(f'Epoch {t} train loss: {loss.item()}')
+
+        train_hist[t] = loss.item()
+
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+
+    return model.eval(), train_hist, test_hist
+
+model = CoronaProphet(
+  n_features=1,
+  n_hidden=512,
+  seq_len=seq_length,
+  n_layers=2
+)
+model, train_hist, test_hist = train_model(
+  model,
+  X_train,
+  y_train,
+  X_test,
+  y_test
+)
+
+
+plt.plot(train_hist, label="Training loss")
+plt.plot(test_hist, label="Test loss")
+plt.ylim((0, 5))
+plt.legend();
