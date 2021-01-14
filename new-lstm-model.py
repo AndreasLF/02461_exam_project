0197c4fcefe601f8603d3a906f6d7ad7ce646201
Andreas Fiehn <andreas@lyhnefiehn.dk>
2021-01-14 20:46:11 +0100

    Feat: Create feature sequences and targets from the train data

--- new-lstm-model.py
+++ new-lstm-model.py
@@ -11,6 +11,29 @@ from sklearn.preprocessing import MinMaxScaler
 # Import floor function from math module
 from math import floor
 
+def create_sequences(data, seq_length):
+    """ Create sequences from the data 
+    
+    Params: 
+        data (list): The data you want to split in sequences
+        seq_length (int): the length of the sequences
+    
+    Returns:
+        A list of features and a list of targets 
+    """
+    xs = []
+    ys = []
+    for i in range(len(data)-seq_length-1):
+        # Create a sequence of features
+        x = data[i:(i+seq_length)]
+        # Take the next value as a target
+        y = data[i+seq_length]
+        # Append to lists
+        xs.append(x)
+        ys.append(y)
+    # Convert to ndarrays and return
+    return np.array(xs), np.array(ys)
+
 # Load flight data from seaborn library
 flight_data = sns.load_dataset("flights")
 # Convert monthly passengers to float
@@ -29,7 +52,9 @@ scaler = MinMaxScaler(feature_range=(-1, 1))
 # Scale data. Data is fit in the range [-1,1]
 train_data_normalized = scaler.fit_transform(train_data .reshape(-1, 1))
 
-# Create tensors from data
-train_data_normalized = torch.FloatTensor(train_data_normalized).view(-1)
+# Create feature sequences and targets
+x_train, y_train = create_sequences(train_data_normalized, 12)
 
-print(train_data_normalized)
+# Convert to tensors
+x_train = torch.Tensor(np.array(x_train))
+y_train = torch.Tensor(np.array(y_train))
